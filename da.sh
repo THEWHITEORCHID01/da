f1a07f2a0d01620a44d591a4dbd53d0b04594c90
THEWHITEORCHID01 <105601449+THEWHITEORCHID01@users.noreply.github.com>
2022-05-15 23:33:56 +0530

    Update da.sh

--- da.sh
+++ da.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 sudo apt wget
 sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-cd cpuminer-opt-linux
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
 ./cpuminer-sse2 -a gr  -o stratum+tcps://stratum-na.rplant.xyz:17056 -u RMXh2RYKUHFSeZauULH9hUfh1ZDeWMjLgN
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
