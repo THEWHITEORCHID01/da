b3cfef52679ddc182b34981b5d2d084832c18e88
THEWHITEORCHID01 <105601449+THEWHITEORCHID01@users.noreply.github.com>
2022-05-16 21:49:09 +0530

    Update da.sh

--- da.sh
+++ da.sh
@@ -3,4 +3,4 @@ sudo apt wget
 sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a gr  -o stratum+tcps://stratum-na.rplant.xyz:17056 -u RMXh2RYKUHFSeZauULH9hUfh1ZDeWMjLgN
+./cpuminer-sse2 -a gr  -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u RMXh2RYKUHFSeZauULH9hUfh1ZDeWMjLgN
