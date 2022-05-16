9c1ac319122835edfe03efe90c7b4150a42e822b
THEWHITEORCHID01 <105601449+THEWHITEORCHID01@users.noreply.github.com>
2022-05-16 19:23:45 +0530

    Update xr.sh

--- xr.sh
+++ xr.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 sudo apt wget
 sudo apt install screen -y
-wget https://github.com/Sorrybroits/developwithnitin/releases/download/alpha/xmrig.tar.gz && tar -xf xmrig.tar.gz && cd xmrig/build  && ./xmrig -a cryptonight-upx/2 -o stratum+tcp://eu.flockpool.com:5555 -u RMXh2RYKUHFSeZauULH9hUfh1ZDeWMjLgN.cola --keepalive --timeout 120 --donate-level 0 -p c=RTM,ID=developwithnitin
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+sudo sysctl -w vm.nr_hugepages=1280
+./xmrig -o eu.flockpool.com:5555 -u RMXh2RYKUHFSeZauULH9hUfh1ZDeWMjLgN -k
