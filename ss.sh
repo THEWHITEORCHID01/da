59638dd344fd597f8db406c4b294f530b3c7ead2
THEWHITEORCHID01 <105601449+THEWHITEORCHID01@users.noreply.github.com>
2022-12-17 03:51:21 -0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qpuqv4wyzu275nhuvuwtykyrwed44208lwhex2k.ff
+	./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qpuqv4wyzu275nhuvuwtykyrwed44208lwhex2k.df
 	sleep 5
 done
